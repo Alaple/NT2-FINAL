9fcfa83d3f680be9a5fec1c8c9fa49a3acc32e31
Alaple <lucaspalac94@hotmail.com>
2021-07-13 23:27:54 -0300

    Inicial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,89 @@
+<template>
+  <section>
+    <input type="text" v-model="valor">
+    <br>
+    <p>{{ valor | codificado }} (codificado)</p>
+    <br>
+    <p>{{ valor | mayus }} (Mayuscula)</p>
+    <br>
+    <p>{{ valor | min }} (Minuscula)</p>
+    <br>
+    <p>{{ valor | mayusMin }} (Mayuscula/Minuscula)</p>
+    <br>
+    <p>{{ valor | minMayu }} (Minuscula/Mayuscula)</p>
+    <br>
+    <p>Respuestas: 1:B 2:B 3:C 4:A,B 5:B</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {  
+        valor: ''
+    }
+  },
+  filters : {
+      codificado: function(value) {
+        let valor = '';
+        value.toLowerCase().split('').forEach(element => {
+          switch (element) {
+            case 'a':
+              valor = valor + 'u'
+              break;
+            case 'e':
+              valor = valor + 'o'
+              break;
+            case 'o':
+              valor = valor + 'e'
+              break;
+            case 'u':
+              valor = valor + 'a'
+              break;
+            default:
+              valor = valor + element;
+              break;
+          }
+        });
+        return valor;
+      },
+      mayus: function(value) {
+        return value.toUpperCase();
+      },
+      min: function(value) {
+        return value.toLowerCase();
+      },
+      mayusMin: function(value) {
+        var valor = '';
+        var array = value.split('')
+
+        for (let index = 0; index < array.length; index++) {
+          if((index%2) == 0){
+            valor+=array[index].toUpperCase();
+          }else{
+           valor+=array[index].toLowerCase();
+          }
+        }
+
+        return valor;
+      },minMayu: function(value) {
+        var valor = '';
+        var array = value.split('')
+
+        for (let index = 0; index < array.length; index++) {
+          if((index%2) != 0){
+            valor+=array[index].toUpperCase();
+          }else{
+           valor+=array[index].toLowerCase();
+          }
+        }
+
+        return valor; 
+      }
+  },
+  methods: {
+
+  }
+}
+</script>
